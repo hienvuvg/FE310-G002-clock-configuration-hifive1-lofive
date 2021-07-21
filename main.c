8581b1650d4d11ff622ffe46373757a0a4f509f3
hienvuvg <60267498+hienvuvg@users.noreply.github.com>
2021-07-21 13:44:04 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -18,27 +18,29 @@ unsigned int cpu_clock;
 int print_mode = 0, count = 0;
 
 
-	void GPIOSetup(){
-		gpio = metal_gpio_get_device(0);
+void GPIOSetup(){
+	gpio = metal_gpio_get_device(0);
 
-		metal_gpio_disable_input(gpio, GPIO_PIN_9);
-		metal_gpio_disable_input(gpio, GPIO_PIN_18);
-		metal_gpio_disable_output(gpio, GPIO_PIN_22);
+	metal_gpio_disable_input(gpio, GPIO_PIN_9);
+	metal_gpio_disable_input(gpio, GPIO_PIN_18);
+	metal_gpio_disable_output(gpio, GPIO_PIN_22);
 
-		metal_gpio_disable_pinmux(gpio, GPIO_PIN_9);
-		metal_gpio_disable_pinmux(gpio, GPIO_PIN_18);
-		metal_gpio_disable_pinmux(gpio, GPIO_PIN_22);
+	metal_gpio_disable_pinmux(gpio, GPIO_PIN_9);
+	metal_gpio_disable_pinmux(gpio, GPIO_PIN_18);
+	metal_gpio_disable_pinmux(gpio, GPIO_PIN_22);
 
-		metal_gpio_enable_output(gpio, GPIO_PIN_9);
-		metal_gpio_enable_output(gpio, GPIO_PIN_18);
-		metal_gpio_enable_input(gpio, GPIO_PIN_22);
-
+	metal_gpio_enable_output(gpio, GPIO_PIN_9);
+	metal_gpio_enable_output(gpio, GPIO_PIN_18);
+	metal_gpio_enable_input(gpio, GPIO_PIN_22);
 }
 
 int main() {
 	cpu_clock_config(64);
-	delay(10000);
 	uart_init(BAUDRATE_115200);
+	
+	cpu_clock = metal_clock_get_rate_hz(&__metal_dt_clock_4.clock);
+	printf("Clock frequency: %d\n", cpu_clock);
+				
 	GPIOSetup();
 
 	while(1){
@@ -54,6 +56,7 @@ int main() {
 				cpu_clock_config(320);
 				uart_init(BAUDRATE_115200);
 				cpu_clock = metal_clock_get_rate_hz(&__metal_dt_clock_4.clock);
+				printf("Clock frequency: %d\n", cpu_clock);
 			}
 		}
 
@@ -63,10 +66,7 @@ int main() {
 			count = 0;
 			metal_gpio_toggle_pin(gpio, GPIO_PIN_18);
 
-			//cpu_clock = metal_clock_get_rate_hz(&__metal_dt_clock_4.clock);
-			//printf("Clock %d\n", cpu_clock);
-
-			printf("Damn\n");
+			printf("Test\n");
 		}
 
 	}
